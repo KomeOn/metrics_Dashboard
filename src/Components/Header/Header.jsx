662cfe88c8078b178034b73a767ca75b9700b79b
Soha <fatima.soha@indiamart.com>
2021-05-21 14:29:46 +0530

    click view more

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header(props) {
         <>
             <nav class="nav-extended indigo darken-3">
                 <div class="nav-wrapper">
-                    <a href="#" class="brand-logo">Logo</a>
+                    <a href="#" class="brand-logo">Metrics Dashboard</a>
                     <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <li><a href="sass.html">Sass</a></li>
--- src/Components/HistoricalComponent/historical_data_table.jsx
+++ src/Components/HistoricalComponent/historical_data_table.jsx
@@ -1,46 +1,47 @@
 import React from 'react';
 
 export default class HistoricalDataTable extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            meta: [],
-            data: [],
-        }
-        this.tableHeader = this.tableHeader.bind(this)
-        // this.tableData = this.tableData.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      meta: [],
+      data: [],
+      clickExpand : false
     }
-    
-    tableHeader() {
-            let val = Object.keys(this.props.data)[0]
-            let arr = this.props.data[val] || {}
-            let JsonArr = []
-            JsonArr.push(Object.keys(arr))
-            let index = [];
-            index = JsonArr[0].map((keys) => {
-                    return keys.slice(3).toUpperCase()
-                })
-            if(index.length > 0)
-            index.unshift("DATE")
-            return index;
-            }
+    this.tableHeader = this.tableHeader.bind(this)
+    // this.tableData = this.tableData.bind(this)
+  }
 
-    tableData() {
-        let dataArr = [];
-        console.log(this.props.data)
-        for (const key in this.props.data) {
-            let temp = []; 
-            temp.push(key+"");  
-            for (const val in this.props.data[key]){
-                let i= this.props.data[key][val].indexOf(":")
-                let str = this.props.data[key][val].substr(i+1);
-                temp.push(str);
-            }
-            dataArr.push(temp);
-        }
-        console.log("DataArr", dataArr);
-        return dataArr;
+  tableHeader() {
+    let val = Object.keys(this.props.data)[0]
+    let arr = this.props.data[val] || {}
+    let JsonArr = []
+    JsonArr.push(Object.keys(arr))
+    let index = [];
+    index = JsonArr[0].map((keys) => {
+      return keys.slice(3).toUpperCase()
+    })
+    if (index.length > 0)
+      index.unshift("DATE")
+    return index;
+  }
+
+  tableData() {
+    let dataArr = [];
+    console.log(this.props.data)
+    for (const key in this.props.data) {
+      let temp = [];
+      temp.push(key + "");
+      for (const val in this.props.data[key]) {
+        let i = this.props.data[key][val].indexOf(":")
+        let str = this.props.data[key][val].substr(i + 1);
+        temp.push(str);
+      }
+      dataArr.push(temp);
     }
+    console.log("DataArr", dataArr);
+    return dataArr;
+  }
 
   render() {
     this.tableData()
@@ -48,35 +49,60 @@ export default class HistoricalDataTable extends React.Component {
     let rowValues = this.tableData();
     let tableRows = [];
     if (rowValues) {
-      rowValues.map((row) =>{
+      rowValues.map((row) => {
         let r = [];
         tableRows.push(<tr>
           {row.map((r) => (<td>{r}</td>))}
         </tr>)
       })
     }
+    let first5 = [];
+    let first5table = []
+    if (rowValues) {
+      first5 = rowValues.slice(0, 5).map((row) => {
+        let r = [];
+        first5table.push(<tr>
+          {row.map((r) => (<td>{r}</td>))}
+        </tr>)
+      })
+      // first5table.push(<tr>
+      //   <td></td>
+      //   <td></td>
+      //   <td></td>
+      //   <td></td>
+      //   <td></td>
+      //   <td></td>
+      //   <td></td>
+      //   <td class="btn-floating btn-large waves-effect waves-light red" onClick={()=>console.log("CLicked view more")}><i class="material-icons">expand_more</i></td>
+      //   <td onClick={()=>console.log("CLicked view more")}><i class="material-icons">expand_more</i></td>
+      //   </tr>)
+    }
     return (
       <>
-      <div className="container" style={{"marginTop": "35px"}}>
-    
-       <div class="card horizontal">
-      <div class="card-stacked">
+        <div className="container" style={{ "marginTop": "35px" }}>
 
-        <table class="centered responsive-table highlight">
-          <thead className="indigo darken-3">
-            <tr style={{"color":"white" }}>
-              {columnHeaders.map((item) => (
-                <th>{item}</th>
-                ))}
-            </tr>
-          </thead>
-          <tbody>
-            {tableRows}
-          </tbody>
-        </table>
-                </div>
-                </div>
-                </div>
+          <div class="card horizontal">
+            <div class="card-stacked">
+
+              <table class="centered responsive-table highlight">
+                <thead className="indigo darken-3">
+                  <tr style={{ "color": "white" }}>
+                    {columnHeaders.map((item) => (
+                      <th>{item}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.clickExpand ? tableRows : first5table}
+                  {/* {first5table} */}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <a class="btn-floating btn-large waves-effect waves-light red center" 
+          onClick={() => {this.setState({clickExpand : !this.state.clickExpand})}}>
+            <i class="material-icons">expand_more</i></a>
       </>
     );
   }
